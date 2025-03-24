776419955b0719a296d14ea78a80d8eeb1857675
JohanGil1502 <johan.gil@uptc.edu.co>
2025-03-23 19:56:54 -0500

    feature: Add initial files

---
+++ src/test/java/com/final_project/grammar_manager/GrammarManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.grammar_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammarManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
